f312e106bf1746ed1567d27286d2471ef5bf0538
Omar Ismail <omariu_dbpro@outlook.com>
2021-06-24 08:46:29 +0300

    init commit

---
+++ src/Locations.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { loadModules } from "esri-loader";
+
+const Locations = (props) => {
+  const view = props.view;
+
+  useEffect(() => {
+    console.log("--");
+    loadModules(["esri/Graphic"]).then(([Graphic]) => {
+      const geo = {
+        type: "point",
+        x: 30.1,
+        y: 30,
+      };
+
+      const geo2 = {
+        type: "point",
+        x: 30.1,
+        y: 32,
+      };
+
+      const sym = {
+        type: "simple-marker",
+        color: "red",
+        size: "15",
+        outline: null,
+      };
+
+      const sym2 = {
+        type: "simple-marker",
+        color: "green",
+        size: "15",
+        outline: null,
+      };
+
+      let grph = new Graphic({
+        geometry: geo,
+        symbol: sym,
+      });
+
+      let grph2 = new Graphic({
+        geometry: geo2,
+        symbol: sym2,
+      });
+      view.graphics.addMany([grph, grph2]);
+
+      view.goTo([grph, grph2]);
+    });
+  }, []);
+
+  return null;
+};
+
+export default Locations;
